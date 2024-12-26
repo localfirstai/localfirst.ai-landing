1279469250a4588969049202291e68a888739db7
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-12-26 20:58:04 +0530

    Adding Solid Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
+import { SolidQueryDevtools } from "@tanstack/solid-query-devtools";
 import type { Component } from "solid-js";
+
+const queryClient = new QueryClient();
 
 const App: Component = () => {
   return (
-    <main class="bg-black flex items-center justify-center w-screen h-screen text-white text-4xl">
-      Local First AI
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main class="bg-black flex items-center justify-center w-screen h-screen text-white text-4xl">
+        Local First AI
+      </main>
+
+      <SolidQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
